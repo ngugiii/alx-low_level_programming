b0c50265c29de3c2887c444689c3cad096e00503
Erick Ngugi <erickngugi54@gmail.com>
2023-02-20 22:39:29 -0800

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,27 +1,6 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-/**
- * _putchar - writes the character C to std
- * @c: parameter to compare
- * Return: 1 or 0
- */
 int _putchar(char c);
-/**
- * print_alphabet - function to print alphabet
- *
- */
 void print_alphabet(void);
-/**
- * print_alphabet_x10 - print alphabet 10 times
- *
- */
 void print_alphabet_x10(void);
-/**
- *
- * _is lower - prints if c is loweer case
- * @c: character to use
- * Return: 1 or 0
- */
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -32,4 +11,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-#endif
