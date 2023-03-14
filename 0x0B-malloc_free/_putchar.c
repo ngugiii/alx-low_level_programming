ea9592a6aadc5d60d350962ac27ec295e95fa015
Erick Ngugi <erickngugi54@gmail.com>
2023-03-14 06:41:44 -0700

    README, main.h and -putchar.c files

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * REturn: on success 1
+ * on Error, -1 is returned, and errno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
